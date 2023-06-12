7fb513debf89d013b43a877ae38b5e12957e25fc
danyvalenz <jesusvalenzuelag25@gmail.com>
2023-06-12 12:07:37 -0700

    se sube proyecto
    Se sube el proyecto recien creado

---
+++ restartApp/restartApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  restartApp
+//
+//  Created by Daniel Valenzuela on 12/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ restartApp/restartApp/restartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  restartAppApp.swift
+//  restartApp
+//
+//  Created by Daniel Valenzuela on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct restartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
